67a982a5da3f7fe284d76a25a77c09a9fb18e32a
manav95 <manavbiodutta@yahoo.com>
2014-10-18 03:44:09 -0400

    EchoNest API now pulling the songs we need. Uses min tempo, max tempo, and num results. We are limited to 20 accesses per minute at the moment however.

--- app/src/main/java/bebunnies/bebunnies/SongSearch.java
+++ app/src/main/java/bebunnies/bebunnies/SongSearch.java
@@ -1,28 +1,58 @@
 package bebunnies.bebunnies;
 
-import com.echonest.api.v4.Artist;
 import com.echonest.api.v4.EchoNestAPI;
 import com.echonest.api.v4.EchoNestException;
 import com.echonest.api.v4.Song;
 import com.echonest.api.v4.SongParams;
-import com.echonest.api.v4.YearsActive;
+import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
+
 /**
- * Created by manavdutta1 on 10/18/14.
+ * @author: Manav Dutta
+ * @version: 2.0
+ * This is the class that pulls songs from EchoNest
  */
 public class SongSearch {
     private EchoNestAPI en;
     private static String APIKey = "TSPV7YH3UINWQAH6T";
+
+    /**
+     * This constructs a new song search instance
+     */
     public SongSearch() {
         en = new EchoNestAPI(APIKey);
     }
-    public List<Song> searchSongsByTempo(int results)
+
+    /**
+     * This does the whole searching. It searches for songs by tempo.
+     * @param min: the min tempo we want
+     * @param max: the max tempo we want
+     * @param results: the results we obtain
+     * @return the list of songs
+     * @throws EchoNestException
+     */
+    public List<Song> searchSongsByTempo(int min, int max, int results)
             throws EchoNestException {
         SongParams p = new SongParams();
-        p.setMinTempo(0);
-        p.setMaxTempo(100);
+        p.setMinTempo(min);
+        p.setMaxTempo(max);
         p.setResults(results);
         List<Song> songs = en.searchSongs(p);
+        Collections.sort(songs, new Comparator<Song>() {
+            public int compare(Song o1, Song o2) {
+                 if (o1 == null || o2 == null) {
+                     throw new NullPointerException();
+                 }
+                 try {
+                         return (int) (o1.getTempo() - o2.getTempo());
+                 }
+                 catch (EchoNestException e) {
+                         System.out.println(e.getMessage());
+                  }
+                return 0;
+            }
+        });
         for (Song song : songs) {
             System.out.printf("%.0f %s %s\n", song.getTempo(), song
                     .getArtistName(), song.getTitle());
@@ -32,7 +62,7 @@ public class SongSearch {
     public static void main(String [] args) {
         SongSearch search = new SongSearch();
         try {
-            List<Song> songs = search.searchSongsByTempo(45);
+            List<Song> songs = search.searchSongsByTempo(50, 450, 20);
             for (Song song : songs) {
                 System.out.printf("%.0f %s %s\n", song.getTempo(), song
                         .getArtistName(), song.getTitle());
