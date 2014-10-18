dfa06976f0fe158b99c474bb9de67fe23848e650
Nathan <nburn42@gmail.com>
2014-10-18 15:03:25 -0400

    Merged

--- app/src/main/java/bebunnies/bebunnies/MainWindow.java
+++ app/src/main/java/bebunnies/bebunnies/MainWindow.java
@@ -1,7 +1,47 @@
 package bebunnies.bebunnies;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
- * Created by nburn42 on 10/18/14.
+ * A placeholder fragment containing a simple view.
  */
-public class MainWindow {
+public class MainWindow extends Fragment {
+    /**
+     * The fragment argument representing the section number for this
+     * fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    public MainWindow() {
+    }
+
+    /**
+     * Returns a new instance of this fragment for the given section
+     * number.
+     */
+    public static MainWindow newInstance() {
+        MainWindow fragment = new MainWindow();
+        Bundle args = new Bundle();
+        //args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        //fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main_drawer, container, false);
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+    }
 }
